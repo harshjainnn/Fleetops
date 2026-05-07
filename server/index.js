ab7434e0c3012e6d8ef3f7227ef36601b6f8900b
Harsh Jain <hjain4815@gmail.com>
2026-05-07 18:51:32 +0530

    Fix Express 5 fallback route

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 // Socket.io setup
